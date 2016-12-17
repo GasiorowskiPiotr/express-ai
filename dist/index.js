e1ea0690993e74dc33d13fae408c85cd056918b6
Piotr Gąsiorowski <gasiorowski.piotr@windowslive.com>
2016-12-17 22:20:52 +0100

    0.1.3

--- dist/index.js
+++ dist/index.js
@@ -6,19 +6,19 @@ var Logger = (function () {
         this.ai = ai;
     }
     Logger.prototype.traceInfo = function (message, properties) {
-        this.ai.trackTrace(message, ContractsModule.SeverityLevel.Information, properties);
+        this.ai.trackTrace(message, 1, properties);
     };
     Logger.prototype.traceError = function (error, message, properties) {
         this.ai.trackException(error, Object.assign({}, { message: message }, properties));
     };
     Logger.prototype.traceWarning = function (message, properties) {
-        this.ai.trackTrace(message, ContractsModule.SeverityLevel.Warning, properties);
+        this.ai.trackTrace(message, 2, properties);
     };
     Logger.prototype.traceVerbose = function (message, properties) {
-        this.ai.trackTrace(message, ContractsModule.SeverityLevel.Verbose, properties);
+        this.ai.trackTrace(message, 0, properties);
     };
     Logger.prototype.traceCritical = function (message, properties) {
-        this.ai.trackTrace(message, ContractsModule.SeverityLevel.Critical, properties);
+        this.ai.trackTrace(message, 4, properties);
     };
     Logger.prototype.trackEvent = function (name, properties) {
         this.ai.trackEvent(name, properties);
