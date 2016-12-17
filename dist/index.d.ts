d9aac954b21b6e3dda38c15176869868d0940089
Piotr Gąsiorowski <gasiorowski.piotr@windowslive.com>
2016-12-17 22:39:19 +0100

    Improvements in build process

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="express" />
-import * as express from 'express';
-declare var _default: (app: express.Application, instrumentationKey: string) => {
-    logErrors: (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => void;
-    logRequest: (req: express.Request, res: express.Response, next: express.NextFunction) => void;
-};
-export = _default;
